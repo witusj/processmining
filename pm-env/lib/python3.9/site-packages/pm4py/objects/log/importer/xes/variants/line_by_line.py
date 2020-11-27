a4cb0aad5cf35a24bd1c93acc418ddbe08eb30bf
Witold ten Hove <witoldtenhove@Witolds-MacBook-Pro.local>
2020-11-27 17:21:47 +0100

    First commit

---
+++ pm-env/lib/python3.9/site-packages/pm4py/objects/log/importer/xes/variants/line_by_line.py
@@ -0,0 +1,125 @@
+import os
+import sys
+from enum import Enum
+
+from pm4py.objects.log.log import EventLog, Trace, Event
+from pm4py.objects.log.util import sorting
+from pm4py.util import constants, xes_constants, exec_utils
+from pm4py.util.dt_parsing import parser as dt_parser
+
+
+class Parameters(Enum):
+    TIMESTAMP_SORT = "timestamp_sort"
+    TIMESTAMP_KEY = constants.PARAMETER_CONSTANT_TIMESTAMP_KEY
+    REVERSE_SORT = "reverse_sort"
+    MAX_TRACES = "max_traces"
+    MAX_BYTES = "max_bytes"
+    SKIP_BYTES = "skip_bytes"
+
+
+def apply(filename, parameters=None):
+    return import_log(filename, parameters)
+
+
+def __fetch_param_value(param, params):
+    return params[param] if param in params else param.value
+
+
+def import_log(filename, parameters=None):
+    """
+    Import a log object from a XML file
+    containing the traces, the events and the simple attributes of them
+
+    Parameters
+    -----------
+    filename
+        XES file to parse
+    parameters
+        Parameters of the algorithm, including
+            Parameters.TIMESTAMP_SORT -> Specify if we should sort log by timestamp
+            Parameters.TIMESTAMP_KEY -> If sort is enabled, then sort the log by using this key
+            Parameters.REVERSE_SORT -> Specify in which direction the log should be sorted
+            Parameters.MAX_TRACES -> Specify the maximum number of traces to import from the log (read in order in the XML file)
+            Parameters.MAX_BYTES -> Maximum number of bytes to read
+            Parameters.SKYP_BYTES -> Number of bytes to skip
+
+
+    Returns
+    -----------
+    xes
+        XES file
+    """
+    if parameters is None:
+        parameters = {}
+
+    date_parser = dt_parser.get()
+
+    max_no_traces_to_import = exec_utils.get_param_value(Parameters.MAX_TRACES, parameters, sys.maxsize)
+    timestamp_sort = exec_utils.get_param_value(Parameters.TIMESTAMP_SORT, parameters, False)
+    timestamp_key = exec_utils.get_param_value(Parameters.TIMESTAMP_KEY, parameters,
+                                               xes_constants.DEFAULT_TIMESTAMP_KEY)
+    reverse_sort = exec_utils.get_param_value(Parameters.REVERSE_SORT, parameters, False)
+
+    skip_bytes = exec_utils.get_param_value(Parameters.SKIP_BYTES, parameters, False)
+    max_bytes_to_read = exec_utils.get_param_value(Parameters.MAX_BYTES, parameters, sys.maxsize)
+
+    file_size = os.stat(filename).st_size
+
+    if file_size > max_bytes_to_read:
+        skip_bytes = file_size - max_bytes_to_read
+
+    log = EventLog()
+    tracecount = 0
+    trace = None
+    event = None
+
+    f = open(filename, "r")
+    f.seek(skip_bytes)
+
+    for line in f:
+        content = line.split("\"")
+        if len(content) > 0:
+            tag = content[0].split("<")[-1]
+            if trace is not None:
+                if event is not None:
+                    if len(content) == 5:
+                        if tag.startswith("string"):
+                            event[content[1]] = content[3]
+                        elif tag.startswith("date"):
+                            event[content[1]] = date_parser.apply(content[3])
+                        elif tag.startswith("int"):
+                            event[content[1]] = int(content[3])
+                        elif tag.startswith("float"):
+                            event[content[1]] = float(content[3])
+                        else:
+                            event[content[1]] = content[3]
+                    elif tag.startswith("/event"):
+                        trace.append(event)
+                        event = None
+                elif tag.startswith("event"):
+                    event = Event()
+                elif len(content) == 5:
+                    if tag.startswith("string"):
+                        trace.attributes[content[1]] = content[3]
+                    elif tag.startswith("date"):
+                        trace.attributes[content[1]] = date_parser.apply(content[3])
+                    elif tag.startswith("int"):
+                        trace.attributes[content[1]] = int(content[3])
+                    elif tag.startswith("float"):
+                        trace.attributes[content[1]] = float(content[3])
+                    else:
+                        trace.attributes[content[1]] = content[3]
+                elif tag.startswith("/trace"):
+                    log.append(trace)
+                    tracecount += 1
+                    if tracecount > max_no_traces_to_import:
+                        break
+                    trace = None
+            elif tag.startswith("trace"):
+                trace = Trace()
+    f.close()
+
+    if timestamp_sort:
+        log = sorting.sort_timestamp(log, timestamp_key=timestamp_key, reverse_sort=reverse_sort)
+
+    return log
